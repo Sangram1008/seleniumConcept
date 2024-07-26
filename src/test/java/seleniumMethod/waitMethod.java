e7434755f8231cdd4dd94ea6b9990e914b31468d
Sangram <sangramm@nedbank.co.za>
2024-07-27 01:18:04 +0530

    Selenium WebDriver Wait Methods

--- src/test/java/seleniumMethod/waitMethod.java
+++ src/test/java/seleniumMethod/waitMethod.java
@@ -1,2 +1,39 @@
-package seleniumMethod;public class waitMethod {
+package seleniumMethod;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class waitMethod {
+
+    WebDriver driver = new ChromeDriver();
+
+    void openUrl() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
+
+    void implicitWaitMethod() {
+        this.openUrl();
+    }
+
+    void explicitWaitMethod() {
+        this.implicitWaitMethod();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement firstName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='Username']")));
+        firstName.sendKeys("San123");
+
+        driver.quit();
+    }
+
+    public static void main(String[] args) {
+        waitMethod wm = new waitMethod();
+        wm.explicitWaitMethod();
+    }
 }
