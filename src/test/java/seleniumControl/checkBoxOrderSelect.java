c0841197030c6064ded7640983c4d2a74080b2af
Sangram <sangramm@nedbank.co.za>
2024-08-05 22:23:58 +0530

    Selenium CheckBox Control

--- src/test/java/seleniumControl/checkBoxOrderSelect.java
+++ src/test/java/seleniumControl/checkBoxOrderSelect.java
@@ -15,12 +15,12 @@ public class checkBoxOrderSelect {
         driver.manage().window().maximize();
         driver.get("https://testautomationpractice.blogspot.com/");
     }
-
     void checkBoxOrder() {
 
         // Clicking Last three Check box
         // 1 2 3 4 5 6 7
         // 0 1 2 3 4 5 6
+
         this.openSite();
         List<WebElement> checkBoxLists = driver.findElements(By.xpath("//*[@class='form-check-input'  and @type='checkbox']"));
 
