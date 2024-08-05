65a1fa746d634376943289779bdb59ac45dccacf
Sangram <sangramm@nedbank.co.za>
2024-08-05 22:17:02 +0530

    Selenium Alert Handling

---
+++ src/test/java/seleniumControl/dropDownWithoutSelect.java
@@ -0,0 +1,43 @@
+package seleniumControl;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class dropDownWithoutSelect {
+
+    WebDriver driver = new ChromeDriver();
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+
+    void openSite() {
+        driver.get("https://phptravels.com/demo/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.findElement(By.xpath("//*[@class='nav-item dropdown']")).click();
+
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        for (WebElement x : links) {
+
+            String value = x.getText();
+            System.out.println(value);
+            if (x.getText().equals("Requirements")) {
+                wait.until(ExpectedConditions.elementToBeClickable(x));
+                x.click();
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        dropDownWithoutSelect ds = new dropDownWithoutSelect();
+        ds.openSite();
+    }
+}
---
+++ src/test/java/seleniumControl/selectDropDown.java
@@ -0,0 +1,45 @@
+package seleniumControl;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.Duration;
+import java.util.List;
+
+public class selectDropDown {
+
+    WebDriver driver = new ChromeDriver();
+
+    void openSite() {
+        driver.get("https://testautomationpractice.blogspot.com/");
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebElement dropDownCountry = driver.findElement(By.xpath("//*[@id='country']"));
+        Select countryValue = new Select(dropDownCountry);
+
+        countryValue.selectByValue("india");
+
+        // Getting all the Option from DropDown
+
+        List<WebElement> list = countryValue.getOptions();
+        System.out.println("Country List Size " + " " + list.size());
+
+        // Printing the List Text
+
+        for (WebElement listValue : list) {
+            System.out.println("Country List" + " " + listValue.getText());
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        selectDropDown dd = new selectDropDown();
+        dd.openSite();
+    }
+}
